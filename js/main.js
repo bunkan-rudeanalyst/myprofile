f7267df9902bd8adfd6e154d1d872cf90025a883
tomoki <>
2023-03-07 20:21:00 +0900

    * ナビゲーションバーを実装 * about, products, contactページをファイルのみ追加

---
+++ js/main.js
@@ -0,0 +1,8 @@
+for (const burder of document.getElementsByClassName('navbar-burger')) {
+    const menuId = burder.dataset.target;
+    const menu = document.getElementById(menuId);
+    burder.addEventListener('click', e => {
+        burder.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+}
